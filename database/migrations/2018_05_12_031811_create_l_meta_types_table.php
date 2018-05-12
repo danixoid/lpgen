9c19250946b08c3d030713e6338ae709880c2119
danixoid <danixoid@gmail.com>
2018-05-12 14:16:17 +0600

    meta fix metatypes added

--- database/migrations/2018_05_12_031811_create_l_meta_types_table.php
+++ database/migrations/2018_05_12_031811_create_l_meta_types_table.php
@@ -16,7 +16,7 @@ class CreateLMetaTypesTable extends Migration
         Schema::create('l_meta_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('content');
+            $table->string('content')->nullable();
             $table->timestamps();
         });
 
